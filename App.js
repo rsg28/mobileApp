93fcd4cd5bd664df9830f2f92b41bf38ec5aef58
rsg28 <rsg28@sfu.ca>
2025-03-07 20:42:38 -0800

    Beginning of Part 2

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import AccountScreen from "./app/screens/AccountScreen";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  useEffect(async ()=>{
+    const result = await ImagePicker.requestCameraRollPermissionsAsync();
+  }, [])
+  return <AccountScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
